a3c9fbf53157deb5a2a0c09b00d61370b291e7e4
cbrookscode <colbybrooks@myyahoo.com>
2025-03-20 15:41:40 -0400

    added database

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	config "github.com/cbrookscode/blog_aggregator/internal/config"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
@@ -28,7 +29,6 @@ func (c *commands) register(name string, f func(*state, command) error) {
 	}
 
 	c.cmdnames[name] = f
-	return
 }
 
 // This method runs a given command with the provided state if it exists.
@@ -37,13 +37,16 @@ func (c *commands) run(s *state, cmd command) error {
 	if !exists {
 		return fmt.Errorf("command doesn't exist")
 	}
-	cmd_func(s, cmd)
+	err := cmd_func(s, cmd)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.arguments) == 0 || len(cmd.arguments) > 1 {
-		return fmt.Errorf("only one string input is valid")
+		return fmt.Errorf("can only provide one string to represent login name. please try again. user was not registered for login")
 	}
 
 	err := s.cfg.SetUser(cmd.arguments[0])
@@ -55,31 +58,51 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
-func main() {
+func cli() (int, error) {
+	// Read file to use for app state initialization, and print output of config file to get before snapshot
 	test_struct, err := config.Read()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return 1, err
 	}
 	fmt.Println(test_struct)
 
+	// initialize app state, map of commands, and struct that holds map. Register login command
 	app_state := state{&test_struct}
-	var cmds map[string]func(*state, command) error
+	cmds := make(map[string]func(*state, command) error)
 	mycmds := commands{cmds}
 	mycmds.register("login", handlerLogin)
 
-	arguments := os.Args
-
-	err = test_struct.SetUser("Hilda Brown")
-	if err != nil {
-		fmt.Println(err)
-		return
+	// build command struct based on inputs from user when running program. first arg is always program name, second is assumed to be command name, rest are arguements for command
+	cmd := command{}
+	args := os.Args
+	if len(args) < 2 {
+		return 1, fmt.Errorf("need to provide command when running program")
+	} else if len(args) > 2 {
+		cmd.name = args[1]
+		cmd.arguments = args[2:]
+	} else {
+		cmd.name = args[1]
 	}
 
+	// run command
+	err = mycmds.run(&app_state, cmd)
+	if err != nil {
+		return 1, err
+	}
+
+	// test output to see if config file was changed
 	test_struct, err = config.Read()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return 1, err
 	}
 	fmt.Println(test_struct)
+	return 0, nil
+}
+
+func main() {
+	err_val, err := cli()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(err_val)
+	}
 }
